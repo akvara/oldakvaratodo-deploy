0f45edf8fb9f2f887a2a9890ef7cc2a53b94d7bc
Andrius K <akvara@gmail.com>
2018-12-30 10:13:45 +0200

    Build

---
+++ precache-manifest.2f419066ef834a4d55143246cf59de8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ec5ecab16671bda25043",
+    "url": "/static/js/main.ec5ecab1.chunk.js"
+  },
+  {
+    "revision": "6e5a213e9c8d53f8441b",
+    "url": "/static/js/1.6e5a213e.chunk.js"
+  },
+  {
+    "revision": "ec5ecab16671bda25043",
+    "url": "/static/css/main.aef9a9a3.chunk.css"
+  },
+  {
+    "revision": "f4dc263a0b07683ef333329edac96c39",
+    "url": "/index.html"
+  }
+];
